7c7cbb490400fd0b73ca69c39eccaa3f71584541
Ahmed <fayeda660@gmail.com>
2026-03-04 02:07:11 +0200

    Config Settings

---
+++ backend/app/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Literal
+from pathlib import Path
+
+# env_path = Path(__file__).parent.parent.parent.parent / ".envs" / ".env.local"
+env_path = "../../.envs/.env.local"
+class Settings(BaseSettings):
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    model_config = SettingsConfigDict(
+        env_file=env_path,
+        env_ignore_empty=True,
+        extra="ignore"
+    )
+    
+    API_V1_STR: str = ""
+    PROJECT_NAME: str = ""
+    PROJECT_DESCRIPTION: str = ""
+    SITE_NAME: str = ""
+
+settings = Settings()
